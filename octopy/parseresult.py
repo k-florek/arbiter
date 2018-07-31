ff6083744f859e916aef0e3d3cd2ca2bfd1afc39
Nick Florek <nickflorek@gmail.com>
2018-07-31 16:04:06 -0500

    added result updating

--- octopy/parseresult.py
+++ octopy/parseresult.py
@@ -5,10 +5,12 @@ import os
 import sqlite3
 
 def parseResult(run_id):
-    #init data struct
+    #init data struct TODO develop data structure class to hold information for this
     results_ar = {}
     assem_stats = {}
     sal_sero = {}
+    strep_sero = {}
+    ecoli = {}
     ids = []
     #parse ar
     with open('resistance_analysis.csv','r') as resin:
@@ -52,6 +54,10 @@ def parseResult(run_id):
             assem_stats[id] = None
         if id not in sal_sero:
             sal_sero[id] = None
+        if id not in strep_sero:
+            strep_sero[id] = None
+        if id not in ecoli:
+            ecoli[id] = None
 
     #setup database
     conn = sqlite3.connect('../../db/rundata.db')
@@ -61,16 +67,50 @@ def parseResult(run_id):
     c.execute('''CREATE TABLE if not exists {run_id}
     (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     ISOID TEXT NOT NULL UNIQUE,
-    KRAKEN TEXT,
     AR TEXT,
-    SEROTYPE TEXT,
+    SALTYPE TEXT,
+    STREPTYPE TEXT,
+    ECOLITYPE TEXT,
     STATS TEXT)
     '''.format(run_id=run_id))
 
     #insert into database TODO add checking for existing rows
     for id in ids:
-        c.execute('''INSERT OR IGNORE INTO {run_id}(ISOID,AR,SEROTYPE,STATS) VALUES(?,?,?,?)'''.format(run_id=run_id),(id,results_ar[id],sal_sero[id],assem_stats[id]))
+        c.execute('''INSERT OR REPLACE INTO {run_id}(ISOID,AR,SALTYPE,STREPTYPE,ECOLITYPE,STATS) VALUES(?,?,?,?)'''.format(run_id=run_id),(id,results_ar[id],sal_sero[id],strep_sero[id],ecoli[id],assem_stats[id]))
     #save changes to database
     conn.commit()
     #close the database
+    conn.close()
+
+    #update submission status in octo.db
+    #binary status code for runs:
+    #[fastqc,kraken,sal,ecoli,strep,ar] = "000000"
+    #0 - not run
+    #1 - submitted
+    #2 - finished
+    conn = sqlite3.connect('../../db/octo.db')
+    c = conn.cursor()
+    for id in ids:
+        c.execute('''SELECT * FROM {run_id} WHERE ISOID=?'''.format(run_id=run_id),[id])
+        row = c.fetchone()
+        statuscode = row[2]
+
+        #update status code to change submitted to finished
+        newcode = ''
+        c = 0
+        for code in statuscode:
+            if c < 2:
+                newcode += code
+                c += 1
+            elif code == 1:
+                newcode += '2'
+                c += 1
+            else:
+                newcode += code
+                c += 1
+
+        c.execute('''UPDATE {run_id} SET STATUSCODE = ? WHERE ISOID = ?'''.format(run_id=run_id),[newcode,id])
+
+    #save and close
+    conn.commit()
     conn.close()
