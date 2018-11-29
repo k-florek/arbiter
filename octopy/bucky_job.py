53f955cbac4708bb2199e280bb7fee32ff277570
Nick Florek <nickflorek@gmail.com>
2018-11-29 11:59:03 -0600

    added staging and preprocessing

--- octopy/bucky_job.py
+++ octopy/bucky_job.py
@@ -4,6 +4,7 @@ import subprocess as sub
 import shutil
 from itertools import zip_longest
 from parseresult import parseResult
+from bucky_prep import preprocess_reads
 import sqlite3
 
 run_id = sys.argv[1]
@@ -36,7 +37,25 @@ with open(job_file,'w') as csvout:
         wr.writerow(row)
 
 #transfer the raw files from storage and preprocess them
+stage_path = os.path.join(config["job_staging_path"],run_id)
+try:
+    os.mkdir(stage_path)
+except FileExistsError:
+    os.rmtree(stage_path)
+    os.mkdir(stage_path)
 
+conn = sqlite3.connect(db_path)
+c = conn.cursor()
+c.execute('''SELECT READ1,READ2 FROM {run_id}'''.format(run_id=run_id))
+rows = c.fetchall()
+conn.close()
+for row in rows:
+    r1 = sub.Popen(['cp',row[0],stage_path])
+    r2 = sub.Popen(['cp',row[1],stage_path])
+    r1.wait()
+    r2.wait()
+
+preprocess_reads(stage_path)
 
 #start up the amazon isntance
 '''
@@ -71,6 +90,45 @@ ec2.instances.filter(InstanceIds=instance_ids).terminate()
 '''
 
 #transfer the files to the amazon instance
+host = 'submit-1.chtc.wisc.edu'
+key = paramiko.RSAKey.from_private_key_file("/home/floreknx/.ssh/id_rsa")
+
+while True:
+    print("Connecting to {}".format(host))
+    i = 0
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=host,username="nwflorek",password='')
+        print("Connected")
+        break
+    except paramiko.AuthenticationException:
+        print("Authentication Failed")
+        sys.exit()
+    except:
+        print("Trying to reconnect..")
+        i +=1
+        time.sleep(2)
+    if 1==5:
+        print("Tried 5 times, quitting.")
+        sys.exit()
+
+stdin,stdout,stderr = ssh.exec_command("ls")
+
+output = stdout.read().decode("utf-8")
+if output:
+    print('there is output')
+print(output)
+
+stdin,stdout,stderr = ssh.exec_command("ls /mnt/gluster/")
+
+output = stdout.read().decode("utf-8")
+if output:
+    print('there is output')
+print(output)
+
+print('Done')
+ssh.close()
 
 #start the bucky pipeline
 
