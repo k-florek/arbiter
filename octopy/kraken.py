3b877d2809c40c305ecacb8c3f8dfeb5ff3e5cb3
Nick Florek <nickflorek@gmail.com>
2018-12-10 13:20:56 -0600

    kraken redesign for aws

--- octopy/kraken.py
+++ octopy/kraken.py
@@ -1,54 +1,139 @@
 #!/usr/bin/env python3
-import csv
-import sys
+import os,sys,shutil,shlex,time
 import subprocess as sub
-import shlex
-import os
-import shutil
-import time
 import sqlite3
+import json
+import boto3
+import paramiko
+
 
 id = sys.argv[1]
 run_id = sys.argv[2]
 path = sys.argv[3]
 
+#get env variables from configuration file
+with open("config.json",'r') as readjson:
+    reader = readjson.read()
+    config = json.loads(reader)
+
 
 #get reads
-for root,dirs,files in os.walk(path):
-    for file in files:
-        if id in file:
-            if '.fastq' in file:
-                if '_R1' in file or '_1' in file:
-                    read1 = os.path.join(path,file)
-                if '_R2' in file or '_2' in file:
-                    read2 = os.path.join(path,file)
-                #TODO add bit for unpaired read
+conn = sqlite3.connect(db_path)
+c = conn.cursor()
+c.execute('''SELECT READ1,READ2 FROM {run_id} WHERE ISOID=?'''.format(run_id=run_id),(id,))
+rows = c.fetchall()
+conn.close()
+read1_path = rows[0][0]
+read2_path = rows[0][1]
+read1 = os.path.basename(read1_path)
+read2 = os.path.basename(read2_path)
 
 #check output path
-if os.path.exists('public/results/'+run_id+'/kraken'):
-    os.chdir('public/results/'+run_id+'/kraken')
-else:
+if not os.path.exists('public/results/'+run_id):
     os.mkdir('public/results/'+run_id)
-    os.mkdir('public/results/'+run_id+'/kraken')
-    os.chdir('public/results/'+run_id+'/kraken')
+    os.mkdir('public/results/'+run_id+'/'+id)
+if not os.path.exists('public/results/'+run_id+'/'+id):
+    os.mkdir('public/results/'+run_id+'/'+id)
 
-#kraken command
+#start aws instance
+print('Starting AWS instance for {runid}'.format(runid=run_id))
+ec2 = boto3.resource('ec2')
+
+BDM = [{"DeviceName": "/dev/sda1",
+"Ebs": {
+"DeleteOnTermination": True,
+"VolumeType": "gp2",
+"VolumeSize": 100}}]
+
+instance_id = ec2.create_instances(
+    BlockDeviceMappings=BDM,
+    ImageId="ami-02a5aab546fb4d3c4",
+    MinCount=1,
+    MaxCount=1,
+    InstanceType="c4.2xlarge",
+    KeyName='octopodes',
+    SecurityGroups=['program_access',]
+)[0].id
+instance = ec2.Instance(instance_id)
+#ssh and transfer reads
+
+#check to see if instance is running
+while True:
+    state = instance.state['Code']
+    if state<16:
+        state = instance.state['Code']
+        time.sleep(10)
+    else:
+        break
+
+#transfer the files to the amazon instance
+host = instance.public_dns_name
+key = paramiko.RSAKey.from_private_key_file(config["aws_key"])
+
+while True:
+    print("Connecting to AWS instance: {}".format(host))
+    i = 0
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=host,username="ubuntu",pkey=key)
+        print("Connected to {}".format(host))
+        break
+    except paramiko.AuthenticationException:
+        print("Authentication Failed")
+        sys.exit()
+    except:
+        print("Trying to reconnect..")
+        i +=1
+        time.sleep(2)
+    if 1==5:
+        print("Tried 5 times, quitting.")
+        sys.exit()
+
+
+#setup for sftp
+#compress the raw reads folder
+print('Transfering reads to AWS instance {aws}'.format(aws=instance_id))
+ftp_client = ssh.open_sftp()
+ftp_client.put(read1_path,'/home/ubuntu/{read1}'.format(read1=read1))
+ftp_client.put(read2_path,'/home/ubuntu/{read2}'.format(read2=read2))
+ftp_client.close()
+
+#start kraken
 cmd_str = '''kraken2 --db bacteria_180803
     --threads 10
     --output {0}.kraken.out
     --paired
     --gzip-compressed {1} {2}'''.format(id,read1,read2)
-cmd = shlex.split(cmd_str)
-kraken = sub.Popen(cmd,stdout=sub.PIPE,stderr=sub.PIPE).wait()
+
+stdin,stdout,stderr = ssh.exec_command(cmd_str)
+exit_status = stdout.channel.recv_exit_status()          # Blocking call
+if exit_status == 0:
+    print ("Finished Kraken2")
+else:
+    print("Error", exit_status)
+
+#transfer results back
+ftp_client = ssh.open_sftp()
+ftp_client.get('/home/ubuntu/{id}.kraken.out'.format(id=id),'public/results/{run_id}/{id}/{id}.kraken.out'.format(run_id=run_id,id=id))
+ftp_client.close()
+
+#close ssh and shutdown instance
+print("Disconnecting from {}".format(host))
+ssh.close()
+print('Shutting down',instance_id)
+ec2.instances.filter(InstanceIds=[instance_id]).terminate()
 
 #run krona
-cmd_str = "../../../../lib/KronaTools-2.7/bin/ktImportTaxonomy -t 3 -q 2 {0}.kraken.out -o {0}_krona.html".format(id)
+lib_path = config["lib_path"]
+krona_path =  os.path.join(lib_path,'KronaTools-2.7/bin/ktImportTaxonomy')
+cmd_str = "{k_path} -t 3 -q 2 {id}.kraken.out -o {id}_krona.html".format(k_path=krona_path,id=id)
 cmd = shlex.split(cmd_str)
-kraken = sub.Popen(cmd,stdout=sub.PIPE,stderr=sub.PIPE).wait()
+sub.Popen(cmd,cwd='public/results/'+run_id+'/'+id).wait()
 
 #update database
-#setup database
-conn = sqlite3.connect('../../../../db/octo.db')
+db_path = os.path.join(config["db_path"],'octo.db')
+conn = sqlite3.connect(db_path)
 c = conn.cursor()
 
 #update submission status in octo.db
@@ -56,13 +141,15 @@ c = conn.cursor()
 #[fastqc,kraken,sal,ecoli,strep,ar] = "000000"
 #0 - not run
 #1 - submitted
-#2 - finished
+#2 - in progress
+#3 - finished
+#4 - error
 c.execute('''SELECT * FROM {run_id} WHERE ISOID=?'''.format(run_id=run_id),(id,))
 row = c.fetchone()
 statuscode = row[2]
-kraken_path = '/results/{run_id}/kraken/{id}_krona.html'.format(run_id=run_id,id=id)
+kraken_path = '/results/{run_id}/{id}/{id}_krona.html'.format(run_id=run_id,id=id)
 if statuscode[1] == '1':
-    newcode = statuscode[0] + '2' + statuscode[2:]
+    newcode = statuscode[0] + '3' + statuscode[2:]
     c.execute('''UPDATE {run_id} SET STATUSCODE = ?,KRAKEN = ? WHERE ISOID = ?'''.format(run_id=run_id),(newcode,kraken_path,id))
 
 #save changes to database
