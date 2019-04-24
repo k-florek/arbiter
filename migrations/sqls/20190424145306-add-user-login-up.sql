fd4bb91e08b46eb4c305de6ac79fe51bfc6a79cf
Kelsey Florek <nickflorek@gmail.com>
2019-04-24 16:44:16 -0500

    added missing ;

--- migrations/sqls/20190424145306-add-user-login-up.sql
+++ migrations/sqls/20190424145306-add-user-login-up.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
 	username	TEXT NOT NULL UNIQUE,
 	password	TEXT NOT NULL,
 	PRIMARY KEY(username)
-)
-INSERT or IGNORE INTO users (username,password) VALUES ('admin','$2b$10$wspkF5Bh1Yx5YwoEfm8S3erXmQGD.ffBnaYbHJWogIiTIjoY.0NEC')
+);
+INSERT INTO `users` VALUES ('admin','$2b$10$wspkF5Bh1Yx5YwoEfm8S3erXmQGD.ffBnaYbHJWogIiTIjoY.0NEC');
